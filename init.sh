d7fe6722a018baccbe7d2d027114d0cd16e30dec
Salih Muhammed <lr0@gmx.com>
2024-03-25 07:30:23 +0200

    Upload shell script

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+replacement_link="$1"
+
+if [ -z "$replacement_link" ]; then
+	echo "Usage: $0 <replacement_link>"
+	exit 1
+fi
+
+find . -type f -exec grep -l -v 'utl' {} + | xargs sed -i "s~github\.com\/tarqeem\/template$~$replacement_link~"
