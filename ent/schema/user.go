d5568922f9e40d5c720fd171698e8406d410c588
Salih Muhammed <lr0@gmx.com>
2024-03-26 12:01:42 +0200

    Update

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -15,16 +15,12 @@ type User struct {
 
 // Fields of the User.
 func (User) Fields() []ent.Field {
-	sp := regexp.MustCompile(`^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$`)
-	ve := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
-	vp := regexp.MustCompile(`^\+?[0-9]{1,3}-?[0-9]{3}-?[0-9]{3}-?[0-9]{4}$`)
-
 	return []ent.Field{
 		String("name"),
-		field.String("password").Sensitive().Match(sp).NotEmpty(),
-		field.String("email").MaxLen(MaxNormalNameLength).NotEmpty().Match(ve).Unique(),
-		field.String("phone").MaxLen(MinNameLen).NotEmpty().Match(vp).Unique(),
-		field.Time("created_at").Default(time.Now),
+		Passowrd(),
+		Email(),
+		Phone(),
+		Created_at(),
 	}
 }
 
--- utl/ent/ent.go
+++ utl/ent/ent.go
@@ -1,14 +1,48 @@
 package ent
 
 import (
+	"regexp"
+	"time"
+
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
 )
 
-const (
+var (
 	MaxNormalNameLength = 300
 	MinNameLen          = 2
+	PassRegex           = regexp.MustCompile(`^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$`)
+	EmailRegex          = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	PhoneRegex          = regexp.MustCompile(`^\+?[0-9]{1,3}-?[0-9]{3}-?[0-9]{3}-?[0-9]{4}$`)
+	CreatedTableName    = "created_at"
+	PasswordTableName   = "passowrd"
+	PhoneTableName      = "phone"
 )
 
-func String(n string) ent.Field      { return field.String(n).MinLen(MinNameLen) }
-func IntPositive(n string) ent.Field { return field.Int(n).Min(0) }
+// return a field `created_at`. Configure name with CreatedTableName
+func Created() ent.Field { return field.Time(CreatedTableName).Default(time.Now) }
+
+// return a string field with `n` name and `MinNameLen` minimum length
+func String(n string) ent.Field { return field.String(n).MinLen(MinNameLen) }
+
+// return a field `password`. Configure name with `CreatedTableName` and Regex
+// with PassRegex
+func Passowrd() ent.Field {
+	return field.String(PasswordTableName).Sensitive().Match(PassRegex).NotEmpty()
+}
+
+// return a non negative integer field
+func NonNegative(n string) ent.Field { return field.Int(n).Min(0) }
+
+// return a field named `phone`. Configure name with `PhoneTableName` and Regex
+// with `PhoneRegex`
+func Phone() ent.Field {
+	return field.String(PhoneTableName).MaxLen(MinNameLen).NotEmpty().Match(PhoneRegex).Unique()
+}
+
+// return a field named `email`. Configure name with `EmailTableName` and Regex
+// with `EmailRegex`
+func Email() ent.Field {
+	return field.String("email").MaxLen(MaxNormalNameLength).
+		NotEmpty().Match(EmailRegex)
+}
