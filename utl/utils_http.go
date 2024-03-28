bbb76e047d70a7daf05ce48af07f741e98a9314f
Salih Muhammed <lr0@gmx.com>
2024-03-28 10:52:09 +0200

    Custom handler and default handler

--- utl/utils_http.go
+++ utl/utils_http.go
@@ -12,24 +12,32 @@ var DefaultErrorHandler func(error, http.ResponseWriter) = func(err error, w htt
 
 var Templates TemplateExecutor
 
-func NewHandler(p HandlerParams) http.HandlerFunc {
-
+// Create a new handler with a custom error handler
+func NewHandler(TemplateName string, Model interface{}, ErrorHandler func(error, http.ResponseWriter)) http.HandlerFunc {
 	if Templates == nil {
 		log.Fatal("You must set default Templates (utl.Templates) value.")
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		err := Templates.ExecuteTemplate(w, p.TemplateName, p.Model)
-		if err != nil && p.ErrorHandler != nil {
-			p.ErrorHandler(err, w)
+		err := Templates.ExecuteTemplate(w, TemplateName, Model)
+		if err != nil && ErrorHandler != nil {
+			ErrorHandler(err, w)
 		} else if err != nil {
 			DefaultErrorHandler(err, w)
 		}
 	}
 }
 
-type HandlerParams struct {
-	TemplateName string
-	Model        interface{}
-	ErrorHandler func(error, http.ResponseWriter)
+// Create a default error handler
+func Handle(TemplateName string, Model interface{}) http.HandlerFunc {
+	if Templates == nil {
+		log.Fatal("You must set default Templates (utl.Templates) value.")
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := Templates.ExecuteTemplate(w, TemplateName, Model)
+		if err != nil {
+			DefaultErrorHandler(err, w)
+		}
+	}
 }
