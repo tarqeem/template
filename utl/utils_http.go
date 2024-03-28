a0e51b25bd80880a14c3c7f12a7a671019515df1
Salih Muhammed <lr0@gmx.com>
2024-03-28 10:45:26 +0200

    Update exector method

--- utl/utils_http.go
+++ utl/utils_http.go
@@ -10,9 +10,16 @@ var DefaultErrorHandler func(error, http.ResponseWriter) = func(err error, w htt
 	http.Error(w, "Internal Server Error: "+err.Error(), 500)
 }
 
+var Templates TemplateExecutor
+
 func NewHandler(p HandlerParams) http.HandlerFunc {
+
+	if Templates == nil {
+		log.Fatal("You must set default Templates (utl.Templates) value.")
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		err := p.Template.ExecuteTemplate(w, p.TemplateName, p.Model)
+		err := Templates.ExecuteTemplate(w, p.TemplateName, p.Model)
 		if err != nil && p.ErrorHandler != nil {
 			p.ErrorHandler(err, w)
 		} else if err != nil {
@@ -25,5 +32,4 @@ type HandlerParams struct {
 	TemplateName string
 	Model        interface{}
 	ErrorHandler func(error, http.ResponseWriter)
-	Template     TemplateExecutor
 }
