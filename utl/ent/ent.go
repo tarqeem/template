02a998418d5971f24bbb9be040f1ca6da08d5e88
Salih Muhammed <lr0@gmx.com>
2024-03-30 09:42:06 +0200

    Update email regex

--- utl/ent/ent.go
+++ utl/ent/ent.go
@@ -14,7 +14,7 @@ var (
 	MaxNormalNameLength = 300
 	MinNameLen          = 2
 	MinPhoneLen         = 9
-	EmailRegex          = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	EmailRegex          = regexp.MustCompile(`[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}`)
 	PhoneRegex          = regexp.MustCompile(`^\+?\d+$`)
 	CreatedTableName    = "created_at"
 	PasswordTableName   = "passowrd"
