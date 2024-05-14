b548bffdc1e8a4eb4b9dcaf724e793a6bd883523
Salih Muhammed <lr0@gmx.com>
2024-05-14 21:14:49 +0300

    Add GetFilesOfExtension

--- utl/fs/fs.go
+++ utl/fs/fs.go
@@ -2,7 +2,11 @@ package fs
 
 import (
 	"embed"
+	"fmt"
+	"os"
 	"path"
+	"sort"
+	"strings"
 )
 
 // return list of fs files under a directory recursively
@@ -32,4 +36,25 @@ func GetFSFilesRecursively(fs *embed.FS, dir string) (out []string, err error) {
 		out = append(out, fp)
 	}
 	return
+}
+
+// get a list of files of some extension, in a directory (not recursive)
+// the "x" paramater must have the "." example: .json.
+func GetFilesOfExtension(dir, x string) ([]string, error) {
+
+	// Open the directory
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var f []string
+
+	// Iterate through the files in the directory
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), x) {
+			f = append(f, file.Name())
+		}
+	}
+	return f, nil
 }
